87feb048082178a7d318128e5c07249b2b85c6f0
Adam Sharp <adam@sharplet.me>
2020-11-03 10:22:44 -0500

    Mark AnyEvent internal

--- Sources/PublisherQueue/AnyEventPublisher.swift
+++ Sources/PublisherQueue/AnyEventPublisher.swift
@@ -1,12 +1,12 @@
 import Combine
 
-public enum AnyEvent {
+enum AnyEvent {
   case output(Any)
   case completion(Subscribers.Completion<Error>)
 }
 
 extension Publisher {
-  public func eraseToAnyEventPublisher() -> AnyPublisher<AnyEvent, Never> {
+  func eraseToAnyEventPublisher() -> AnyPublisher<AnyEvent, Never> {
     map(AnyEvent.output)
       .append(AnyEvent.completion(.finished))
       .catch { error in Just(.completion(.failure(error))) }
